b89943d16b8165b5bd4ca3b070cf482f07922402
tiko-tchumbadze <tinatin.chumbadze048@law.tsu.edu.ge>
2024-12-25 21:14:44 +0400

    მე-7 დავალება

---
+++ scripts/colorchange.js
@@ -0,0 +1,14 @@
+function redColor() {
+    let redColor = document.getElementById("redColor");
+    redColor.style.color = "#a33008" 
+}
+
+function blueColor() {
+    let blueColor = document.getElementById("blueColor");
+    blueColor.style.color = "#324cf1" 
+}
+
+function greenColor() {
+    let greenColor = document.getElementById("greenColor");
+    greenColor.style.color = "#0da206" 
+}
---
+++ scripts/hidecontent.js
@@ -0,0 +1,9 @@
+function hidecontent() {
+    let text = document.getElementById("text");
+    text.style.visibility = "hidden";
+}
+
+function showcontent() {
+    let text = document.getElementById("text");
+    text.style.visibility = "visible";
+}
